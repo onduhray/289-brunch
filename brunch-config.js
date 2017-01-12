016c451f8f8532a1049d2c5792fc7100ccbb3018
André Casey <me@andrecasey.com>
2017-01-12 03:16:39 -0800

    - Changed default scripts to allow easier starting - updated dependencies - fixed config

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,32 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'main.js'},
-  stylesheets: {joinTo: 'main.css'},
+exports.config = {
+  // See http://brunch.io/#documentation for docs.
+  files: {
+    javascripts: {
+      joinTo: "js/main.js"
+    },
+    stylesheets: {
+      joinTo: "css/main.css"
+    },
+    templates: {
+      joinTo: "js/main.js"
+    }
+  },
+  // Configure your plugins
+  plugins: {
+      imageoptimizer: {
+          smushit: true, // if false it use jpegtran and optipng, if set to true it will use smushit
+         imagePath: 'images' // your image path within your public folder
+      }
+  },
+
+    npm: {
+        enabled: true
+    },
+
+    overrides: {
+        testing: {
+            sourceMaps: false
+        }
+  }
 };
